e746cb3748142dbedfdb1c8c52f74abe3dcf5692
Paul Alarcon <pa.f.e@hotmail.com>
2020-08-17 10:45:18 -0400

    Modified UI

--- script/sortingAlgos/bubbleSort.js
+++ script/sortingAlgos/bubbleSort.js
@@ -3,97 +3,90 @@ import * as Canvas from "../utils/canvas.js";
 import * as modal from "../utils/modal.js";
 
 
-  const activeButtons = () => {
-    let finalIteration = canvasBlobs.length - 1;
-    let i = finalIteration;
+let arr = util.generateArray(25, 50);
+let originY = Canvas.height - 20;
+let width = Canvas.width / arr.length;
+let spacing = width * 0.1;
+let heightScale = Canvas.height * 0.1;
+let margin = spacing / 2;
+let canvasArr = [];
 
-    const buttons = document.querySelectorAll(".control");
-    buttons.forEach( b => {
-      b.addEventListener('click', () => {
-        if(b.id == 'prevButton'){
-          if(i > 0) {
-            i = i - 1;
-          Canvas.renderImage(canvasBlobs[i]);
-          }
-        }
-        else if(b.id =='nextButton' ){
-          if(i <= finalIteration){
-          Canvas.renderImage(canvasBlobs[++i]);
-          }
-        }
-        else if(b.id == 'finalButton'){
+const activeButtons = () => {
+  let finalIteration = canvasArr.length - 1;
+  let i = finalIteration;
 
-          i = finalIteration;
-          Canvas.renderImage(canvasBlobs[i]);
+  const buttons = document.querySelectorAll(".control");
+  buttons.forEach((b) => {
+    b.addEventListener("click", () => {
+      if (b.id == "prevButton") {
+        if (i - 1 > 0) {
+          // console.log(finalIteration)
+          // Canvas.paintCanvas("black");
+          // console.log(canvasArr[0]);
+          let temp = --i;
+          drawArray(canvasArr[temp]);
+          console.log(temp);
         }
+      } else if (b.id == "nextButton") {
+        if (i <= finalIteration) {
+          // Canvas.paintCanvas("black");
+          drawArray(canvasArr[++i]);
+        }
+      } else if (b.id == "finalButton") {
+        i = finalIteration;
+        // Canvas.paintCanvas("black");
+        drawArray(canvasArr[i]);
       }
-      )
+    });
+  });
+};
+
+const drawArray = (arr) => {
+  //Repaint canvas everytime
+  Canvas.paintCanvas("black");
+  arr.forEach((element, index) => {
+    let posX = index * width + margin;
+    let rectHeight = heightScale + element * 10;
+    Canvas.ctx.fillStyle = "white";
+    Canvas.ctx.fillRect(
+      posX,
+      originY - rectHeight,
+      width - spacing,
+      rectHeight
+    );
+  });
+};
+
+const bubbleSort = arr => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 1; j < arr.length - i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        util.swap(j - 1, j, arr);
+        //adds on to the canvas array
+        canvasArr.push([...arr]);
+      }
     }
-    )
-
   }
+};
 
-    let arr = util.generateArray(10, 50);
-    // let arr = [20., 22, 3, 4, 5, 7, 8, 11, 12, 22, 23, 44, 6,25];
-    let originY = Canvas.height - 20;
-    let width = Canvas.width/arr.length;
-    let spacing = width * 0.1;
-    let heightScale = Canvas.height * 0.1;
-    let margin = spacing / 2;
+const drawOnCanvas = (arr3d, speed) => {
+  let index = 0;
+  const currentInterval = setInterval(() => {
+    if(index == arr3d.length) {
+      clearInterval(currentInterval);
+    }else{
+      drawArray(arr3d[index++]);
+    }
+  }, speed);
+};
 
-    let canvasBlobs = [];
+const Main  = () => {
+  //TOP SPEED
+  let speed = 1000 / arr.length/2;
 
-    const drawArray = (arr) => {
-        arr.forEach((element, index) => {
-          let posX = index * width + margin;
-          let rectHeight = heightScale + element * 10;
-          Canvas.ctx.fillStyle = "white";
-          Canvas.ctx.fillRect(
-            posX,
-            originY -rectHeight ,
-            width - spacing,
-            rectHeight
-          );
-        });
-      };
+  bubbleSort(arr);
+  drawOnCanvas(canvasArr, speed);
+  activeButtons();
+};
 
-     //bubble sort
-    const bubbleSort  = () => {
-    let i = 0;
-    const currentInterval = setInterval(() => {
-
-      if (i == arr.length){
-        clearInterval(currentInterval);
-        // modal.openModal();
-        console.log(arr);
-        Canvas.canvas.toBlob( c => canvasBlobs.push(c));
-        activeButtons();
-      } 
-
-      for (let j = 1; j < arr.length - i; j++) {
-        if (arr[j - 1] > arr[j]) {
-          util.swap(j - 1, j, arr);
-          //Repaint canvas everytime
-          Canvas.paintCanvas("black");
-          //Draw array to the canvas;
-          drawArray(arr);
-          //Adds the current state of the canvas to the screen
-          Canvas.canvas.toBlob( c => canvasBlobs.push(c));
-        }
-      }
-      i++;
-    }, 1000/15);
-
-  }
-
-
-
-
-bubbleSort();
-
-
-
-
-
-
-
+Main();
